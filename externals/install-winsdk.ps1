a654536d6476408f0a30ad196d31e25f24410f57
nihui <shuizhuyuanluo@126.com>
2025-11-30 19:52:25 +0800

    Add additional Windows SDK options

--- externals/install-winsdk.ps1
+++ externals/install-winsdk.ps1
@@ -7,7 +7,7 @@ param([Parameter(Mandatory=$true, Position=0)]
 $ErrorActionPreference = 'Stop'
 
 # Constants
-$WindowsSDKOptions = @("OptionId.UWPCpp")
+$WindowsSDKOptions = @("OptionId.UWPCpp", "OptionId.DesktopCPPx64", "OptionId.DesktopCPPx86", "OptionID.DesktopCPPARM64")
 $WindowsSDKRegPath = "HKLM:\Software\Microsoft\Windows Kits\Installed Roots"
 $WindowsSDKRegRootKey = "KitsRoot10"
 $WindowsSDKVersion = "10.0.$buildNumber.0"
